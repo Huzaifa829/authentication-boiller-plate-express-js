79596c1455095682bec7dd7781d023185bcdb860
Huzaifa Ahmed <huzaifaahmed829@gmail.com>
2025-02-11 22:37:49 +0500

    asdsad

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
 import cors from "cors";
 import connectDb from "./src/db/index.js";
 import userRoute from "./src/routes/User.routes.js"
