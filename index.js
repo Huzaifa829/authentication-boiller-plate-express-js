0c960dce57ee7e915c9674a0ccf0cd059686b1d9
Huzaifa Ahmed <huzaifaahmed829@gmail.com>
2025-02-11 22:33:47 +0500

    index file chnges

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/",(req, res) => {
 connectDb()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
